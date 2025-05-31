e1ecabbb1f5f18541542096e1693f1e6e9ab9321
Raeena27 <raeena.singh07@gmail.com>
2025-05-31 19:53:55 +0530

    Add files via upload

---
+++ birthdays.py
@@ -0,0 +1,20 @@
+a= "1. Raya"
+q= 20
+b= "2. Veer" 
+r= 15
+c= "3. Kaira" 
+s= 13
+d= "4. Bunny" 
+t= 25 
+e= "5. Lokesh"
+m= 56
+print (a) 
+print (q) 
+print (b) 
+print (r) 
+print (c) 
+print (s) 
+print (d) 
+print (t) 
+print (e) 
+print (m) 
