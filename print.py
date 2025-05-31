4b414c6c0279cd2a9719b1b4cb7a528d92f4864e
Raeena27 <raeena.singh07@gmail.com>
2025-05-31 19:40:39 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,4 @@
+print (2)
+print ("Welcome to the world of coding")
+print ("Raeena", 27)
+print ("Welcome to the world of coding\n")
---
+++ variables.py
@@ -0,0 +1,6 @@
+a= "Raeena"
+b= 27
+print (a)
+print (b)
+age= int(input("Enter your age"))
+print ("the age is", age)
