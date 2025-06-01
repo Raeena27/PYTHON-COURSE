fc229eca405e36e46897a91a9471eb9289136e40
Raeena27 <raeena.singh07@gmail.com>
2025-06-01 20:07:40 +0530

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,16 @@
+string= "word"
+float= 0.2
+integer= 27
+boolean= False 
+print ("string", string)
+print (type (string))
+print ("float", float)
+print (type (float))
+print ("integer", integer)
+print (type(integer))
+print ("boolean", boolean)
+print (type (boolean))
+print ("After type casting")
+float= int(float)
+print (float)
+print (type (float))
