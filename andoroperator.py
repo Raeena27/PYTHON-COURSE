0f77126db1619a42fe13b8173573f8987bb9b579
Raeena27 <raeena.singh07@gmail.com>
2025-06-15 19:39:06 +0530

    Add files via upload

---
+++ andoroperator.py
@@ -0,0 +1,10 @@
+a= 10
+b= 40
+if a>0 and b>0:
+    print ("The numbers are greater than zero")
+x= 30
+y= -15
+if x<0 or y<0:
+    print ("Either of the numbers is less than zero")
+else:
+    print ("No number is less than zero")
