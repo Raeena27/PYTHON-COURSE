7d5b68b4b71e35aa0d7188fcd4c1dba22daeb0da
Raeena27 <raeena.singh07@gmail.com>
2025-07-06 19:46:51 +0530

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,8 @@
+mean= 38
+numbers= 40
+wrong_number= 36
+right_number= 56
+total_sum= mean*numbers
+correct_sum= total_sum-wrong_number+right_number
+correct_mean= correct_sum/numbers
+print ("The correct mean is", correct_mean)
